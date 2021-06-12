c5355d4d1c1758cb6c4f79e79ce885965ce37921
Gabriel Mesquita <gabrielteixeiramesquita@gmail.com>
2021-06-12 12:09:19 -0300

    :rocket: Validando assinatura ativa

--- src/components/SubscribeButton/index.tsx
+++ src/components/SubscribeButton/index.tsx
@@ -1,20 +1,27 @@
-import { signIn, useSession } from 'next-auth/client';
+import { signIn, useSession } from 'next-auth/client'
+import { useRouter } from 'next/dist/client/router'
 
-import { api } from '../../services/api';
-import { getStripeJs } from '../../services/stripe-js';
+import { api } from '../../services/api'
+import { getStripeJs } from '../../services/stripe-js'
 
 import styles from './styles.module.scss'
 
 interface SubscribeButtonProps {
-  priceId: string;
+  priceId: string
 }
 
 export function SubscribeButton({ priceId }: SubscribeButtonProps) {
   const [session] = useSession()
+  const router = useRouter()
 
   async function handleSubscribe() {
     if (!session) {
       signIn('github')
+      return
+    }
+
+    if (session.activeSubscription) {
+      router.push('/posts')
       return
     }
 
